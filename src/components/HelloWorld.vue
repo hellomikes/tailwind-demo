872272a1a626f9766e3c4a8c435467775216b3a1
Mike Smedley <mike.s@helloevery1.com>
2020-07-08 09:21:21 +0100

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="bg-category-alpha md:bg-brand-light lg:bg-category-beta w-10 md:w-20 lg:w-100 h-10 md:h-20 lg:h-100">
+      I should change colours with every breakpoint
+    </div>
+
+    <a
+      href="/link-to-page"
+      class="c-category-link w-full md:w-1/3 md:px-2 mb-2 md:mb-4"
+      ><div class="c-category-link__container md:bg-category-alpha">
+        <div class="c-category-link__container__img border-category-alpha">
+          <img
+            src="http://placehold.it/300x190"
+            alt="See items in Fencing category"
+          />
+        </div>
+        <div class="c-category-link__content">
+          <h4 class="c-category-link__container__title">
+            Fencing
+          </h4>
+          <div class="c-category-link__icon md:mt-4 md:bg-category-alpha">
+            <span>View Products</span>
+            <svg version="1.1" viewBox="0 0 9 14" class="svg-icon svg-fill">
+              <path
+                pid="0"
+                d="M1 1l6 6-6 6"
+                _stroke="#7B5901"
+                stroke-width="2"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <!----></a
+    >
   </div>
 </template>
 
@@ -34,8 +40,8 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 }
 </script>
 
@@ -52,7 +58,124 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+
+</style>
+
+<style lang="css">
+.c-category-link {
+  @apply bg-brand-dark flex text-white uppercase tracking-sm text-lg-button font-bold transition-all items-center justify-between;
+}
+.c-category-link.no-image {
+  @apply p-4 w-full bg-brand-medium;
+}
+
+.c-category-link.no-image .c-category-link__content {
+  @apply w-full;
+}
+.c-category-link:hover {
+  /* @apply pr-1; */
+}
+
+.c-category-link__container {
+  box-shadow: 0 2px 9px rgba(0,0,0,0.13);
+  @apply flex flex-row items-center h-full;
+}
+
+.c-category-link__content {
+  @apply flex flex-row justify-between w-3/4;
+}
+
+.c-category-link__container__title {
+  @apply block text-left text-h5 uppercase leading-small tracking-sm font-body;
+}
+.c-category-link.no-image .c-category-link__container__title {
+  @apply w-full;
+}
+.c-category-link__container__img {
+  @apply border-r-4 inline mr-2 inline-flex w-1/4;
+}
+.c-category-link__container__img img {
+  @apply w-full h-16 object-cover;
+}
+.c-category-link__icon svg {
+  @apply self-center stroke-current fill-transparent ml-1 h-3 mr-3;
+}
+
+.c-category-link__icon {
+  @apply inline-flex;
+}
+
+.c-category-link__icon span {
+  @apply hidden;
+}
+
+@screen md {
+  .c-category-link {
+    @apply normal-case;
+  }
+  .c-category-link.no-image {
+    @apply p-0;
+  }
+  .c-category-link.no-image .c-category-link__content {
+    height: 100%;
+  }
+
+  .c-category-link__content {
+    @apply flex-col w-full py-10 px-2 justify-center;
+  }
+
+  .c-category-link {
+    @apply inline-flex flex-col normal-case bg-transparent justify-start;
+  }
+
+  .c-category-link:hover {
+    /* transform: scale(1.02); */
+  }
+
+  .c-category-link__container {
+    box-shadow: none;
+    @apply flex-col;
+  }
+  .c-category-link__container,
+  .c-category-link__container__title,
+  .c-category-link__container__img {
+    @apply block w-full mr-0 border-r-0;
+  }
+  .c-category-link__container__title {
+    font-size: 1.8125rem;
+    @apply text-center leading-h1 font-black normal-case tracking-normal;
+  }
+
+  .c-category-link__container__img {
+    @apply border-t-4;
+  }
+
+  .c-category-link__container__img img {
+    @apply w-full h-auto;
+  }
+  .c-category-link__icon {
+    @apply flex w-full content-center justify-center;
+  }
+  .c-category-link__icon svg {
+    @apply mr-0;
+  }
+  .c-category-link__icon span {
+    @apply block uppercase;
+  }
+  .c-category-link:hover {
+    /* @apply pr-2; */
+  }
+
+  .c-category-link.no-image {
+    /* @apply py-0; */
+  }
+  .c-category-link.no-image .c-category-link__container {
+    @apply h-full justify-center inline-flex;
+  }
+
+  .c-category-link.is-category-page .c-category-link__container__title {
+    @apply text-lg-h4 font-bold;
+  }
 }
 </style>
+
